120b1700af2eb3621c2afd42b10b6514cffe3240
mlodovico <murilo.lodovico@outlook.com>
2024-05-16 17:39:33 -0300

    feature: auth implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ClientsModule } from './clients/clients.module';
 import { AdminsModule } from './admins/admins.module';
 import { PetsModule } from './pets/pets.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ClientsModule, AdminsModule, PetsModule],
+  imports: [ClientsModule, AdminsModule, PetsModule, AuthModule],
   providers: [],
 })
 export class AppModule {}
