f2157b948d213f580f2661bb5afa384aa94aba3c
mlodovico <murilo.lodovico@outlook.com>
2024-05-20 14:48:19 -0300

    feature: authentication service making globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,32 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { ClientsModule } from './clients/clients.module';
 import { AdminsModule } from './admins/admins.module';
 import { PetsModule } from './pets/pets.module';
 import { AuthModule } from './auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth/auth.controller';
+import { AuthGuard } from './auth/auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Module({
-  imports: [ClientsModule, AdminsModule, PetsModule, AuthModule],
-  providers: [],
+  imports: [
+    ClientsModule,
+    AdminsModule,
+    PetsModule,
+    AuthModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'defaultSecret',
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
