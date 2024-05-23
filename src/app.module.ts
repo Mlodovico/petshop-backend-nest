9e4d0dbd134e4dfc766fcc838cff9090e7cf3e64
mlodovico <murilo.lodovico@outlook.com>
2024-05-23 00:48:09 -0300

    fix: removing auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,12 @@
 import { Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
 import { ClientsModule } from './clients/clients.module';
 import { AdminsModule } from './admins/admins.module';
 import { PetsModule } from './pets/pets.module';
 import { AuthModule } from './auth/auth.module';
-import { JwtModule } from '@nestjs/jwt';
-import { AuthController } from './auth/auth.controller';
-import { AuthGuard } from './auth/auth.guard';
-import { AuthService } from './auth/auth.service';
 
 @Module({
-  imports: [
-    AuthModule,
-    ClientsModule,
-    AdminsModule,
-    PetsModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET || 'defaultSecret',
-      signOptions: { expiresIn: '1h' },
-    }),
-  ],
-  controllers: [AuthController],
-  providers: [
-    AuthService,
-    {
-      provide: APP_GUARD,
-      useClass: AuthGuard,
-    },
-  ],
+  imports: [AuthModule, ClientsModule, AdminsModule, PetsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
